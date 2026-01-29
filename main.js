f04774a445d20fda18f98546bc8792df5d2f1587
Lei <leilachiderski@gmail.com>
2026-01-29 19:25:43 -0300

    app icon changed and default nav removed

--- main.js
+++ main.js
@@ -4,9 +4,16 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-    })
+        frame: false,
+        icon: 'Images/StrawberryCat.png',
+        webPreferences:{
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
 
     win.loadFile('Weather.html');
+    win.setMenuBarVisibility(false);
 }
 
 app.whenReady().then(() => {
