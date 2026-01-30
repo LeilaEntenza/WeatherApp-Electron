2bfa4b8ca65851d939f5c035351346f93c4ca1a4
Lei <leilachiderski@gmail.com>
2026-01-30 20:26:46 -0300

    nav pictures

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 600,
+        height: 500,
         frame: false,
         icon: 'Images/StrawberryCat.png',
         webPreferences:{
